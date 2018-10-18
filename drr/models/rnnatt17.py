c8a700a37d4612eb3d75f2775ec8e4a53e2fbb44
WJ <953372680@qq.com>
2018-10-18 10:47:55 +0800

    RNNAtt17

--- drr/models/rnnatt17.py
+++ drr/models/rnnatt17.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Re-implementation of the paper http://aclweb.org/anthology/P/P17/P17-2040.pdf
-# 'A Recurrent Neural Model with Attention for the Recognition of Chinese Implicit
-#  Discourse Relations'
+# 'A Recurrent Neural Model with Attention for the Recognition of Chinese Implicit Discourse Relations'
 
 import torch
 import torch.nn as nn
@@ -11,7 +10,8 @@ import torch.nn.functional as F
 class RNNAtt17(nn.Module):
 
     def __init__(self, opts):
-        #  torch.nn.Embedding(m, n)  m 表示单词的总数目，n 表示词嵌入的维度
+        # torch.nn.Embedding(m, n)
+        # m represents the total number of words, and n represents the dimension of word embedding
         self.vocab_size = opts['vocab_size']
 
         super(RNNAtt17, self).__init__()
@@ -21,25 +21,31 @@ class RNNAtt17(nn.Module):
             300
         )
 
+        # single layer bidirectional lstm
         self.blstm = nn.LSTM(
             300,
             300,
             batch_first=True,
             bidirectional=True
-        )  # single layer bidirectional lstm
+        )
 
+        # Attention layer
         self.att = nn.Linear(300, 1)
+
+        # 4 represents the number of categories
         self.proj2class = nn.Linear(300, 4)
 
     def forward(self, input):
-        """
+        r"""
 
         Args
         ----------
-        input   : [N, 256] 每批取N组，每组256个单词
-            输入为两个维度(batch的大小，每个batch的单词个数)，输出则在两个维度上加上词向量的大小。
+        input : [N, 256] N groups per batch, 256 words per group
             N is batch size, 256 is the default length of arg1:arg2
-            after padding
+
+            Using fixed-length sequences of 256 tokens with
+            zero padding at the beginning of shorter sequences
+            and truncate longer ones
 
         Returns
         ----------
@@ -49,9 +55,18 @@ class RNNAtt17(nn.Module):
         """
         batch_size = input.size(0)
 
+        # Each word index returns a 300-dimensional vector,
+        # a total of 256 word indexes for a line of text,
+        # a matrix of 256x300,
+        # N rows per batch, and finally a Nx256x300 tensor.
         embs = self.emb(input)  # [N, 256, 300]
+
+        # Bidirectional LSTM gets Nx256x600
+        # front 300 is left to right
+        # rear 300 is right to left
         hids, _ = self.blstm(embs)  # [N, 256, 300 x 2]
 
+        # left to right
         l2r_hids = hids.view(
             batch_size,
             256,
@@ -59,6 +74,7 @@ class RNNAtt17(nn.Module):
             2
         )[:, :, :, 0]
 
+        # right to left
         r2l_hids = hids.view(
             batch_size,
             256,
@@ -69,6 +85,9 @@ class RNNAtt17(nn.Module):
         hids_sum = l2r_hids + r2l_hids  # [N, 256, 300]
         hids_sum_activated = torch.tanh(hids_sum)
         hids_proj = self.att(hids_sum_activated).squeeze(2)  # [N, 256]
+
+        # Dim=0 by column softmax;
+        # Dim=1 by line softmax
         alpha = F.softmax(hids_proj, dim=1)  # [N, 256]
 
         r = torch.bmm(
